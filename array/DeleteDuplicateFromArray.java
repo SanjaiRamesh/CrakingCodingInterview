28ad2d4b7c6a05d0a823ab6dd1aea242a7a863ec
SanjaiRamesh <47880018+SanjaiRamesh@users.noreply.github.com>
2024-12-26 21:56:40 +0530

    Delete duplicate key from array list and array

---
+++ array/DeleteDuplicateFromArray.java
@@ -0,0 +1,56 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class DeleteDuplicateFromArray {
+
+    public static void deleteDuplicate(List<Integer> input) {
+        if(Objects.isNull(input) || input.isEmpty()) {
+            return ;
+        }
+
+        int n = input.size();
+        Integer current = input.get(0);
+        int j = 1;
+        for(int i = 1; i<n; i++) {
+            if(!current.equals(input.get(i))) {
+                input.set(j, input.get(i));
+                current = input.get(i);
+                j++;
+            }
+        }
+
+        for(int k=j-1;k<n-1;k++) {
+            input.remove(j);
+        }
+    }
+
+    public static int solution2(List<Integer> input) {
+
+        int writeIndex = 1;
+        int n = input.size();
+        for( int i = 1; i<n;i++) {
+            if(!input.get(writeIndex-1).equals(input.get(i))) {
+                input.set(writeIndex++, input.get(i));
+            }
+        }
+
+        return writeIndex;
+
+//        for(int i = writeIndex; i<n;i++) {
+//            input.remove(writeIndex);
+//        }
+
+    }
+    public static void main(String[] args) {
+        List<Integer> input = new ArrayList<>(List.of(2,3,5,5,7,11,11,11,13));
+        System.out.println(input);
+        deleteDuplicate(input);
+        System.out.println(input);
+        List<Integer> input1 = new ArrayList<>(List.of(2,2,2,3,5,5,7,11,11,11,13,14,14,14));
+        System.out.println(input1);
+        System.out.println(input1.subList(0,solution2(input1)));
+    }
+}
---
+++ array/DeleteKeyFromArrayAndShift.java
@@ -0,0 +1,53 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class DeleteKeyFromArrayAndShift {
+
+    public static void deleteKeyFromArrayList(List<Integer> input, int key) {
+        if(Objects.isNull(input) || input.isEmpty()) {
+            return;
+        }
+        int writerIdx = 0;
+        int n = input.size();
+
+        for(int i=0;i<n;i++) {
+
+            if(input.get(i).equals(key)) {
+                input.remove(writerIdx);
+                i--;n--;
+            } else {
+                writerIdx++;
+            }
+
+        }
+    }
+
+    public static int deleteFromArray(int[] input, int key) {
+        int n = input.length;
+        int writeIdx = 0;
+        for(int i = 0;i<n;i++) {
+            if( key != input[i]) {
+                input[writeIdx++] = input[i];
+            }
+        }
+
+        for(int i = 0; i<writeIdx; i++) {
+            System.out.print(" "+ input[i]);
+
+        }
+        return writeIdx;
+    }
+    public static void main(String[] args) {
+        List<Integer> input = new ArrayList<>(List.of(2,3,5,5,7,11,11,11,13));
+        System.out.println(input);
+        deleteKeyFromArrayList(input, 11);
+        System.out.println(input);
+        int[] input1 = {2,3,5,5,7,11,11,11,13,13,13,12};
+        System.out.println(Arrays.toString(input1));
+        deleteFromArray(input1, 13);
+    }
+}
